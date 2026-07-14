efae559299346bc67c110b602e9dfac6fa883109
Nicolas Bouème <nicolasboueme@gmail.com>
2026-07-14 15:22:25 +0900

    Add health_check and echo

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use axum::{
-    routing::get,
+    routing::{get, post},
     Router,
 };
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+    .route("/", get(hello_world))
+    .route("/health_check", get(health_check))
+    .route("/echo", post(echo));
     
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap(); 
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    .await
+    .expect("Failed to bind to port 3000");
+
+    axum::serve(listener, app)
+    .await
+    .expect("Server failed to start");
 }
 
-async fn root() -> &'static str {
-    println!("Hello, World!");
-    
+async fn hello_world() -> &'static str {
     "Hello, World!"
+}
+
+async fn health_check() -> &'static str {
+    "OK"
+}
+
+async fn echo(body: String) -> String {
+    format!("You sent: {}", body)
 }
