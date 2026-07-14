382cb3c900720ea4f9b719a0d03423ad93352cc1
Nicolas Bouème <nicolasboueme@gmail.com>
2026-07-14 14:43:55 +0900

    Init project within a Hello World using axum and tokio

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap(); 
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    println!("Hello, World!");
+    
+    "Hello, World!"
+}
