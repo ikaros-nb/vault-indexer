b44eeeaf65c2a707300cfae1d498ff5156c78015
Nicolas Bouème <nicolasboueme@gmail.com>
2026-07-19 17:57:00 +0900

    Add /webhook endpoint which print received json object

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use axum::{
+    http::StatusCode,
     routing::{get, post},
+    Json,
     Router,
 };
+
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/", get(hello_world))
-    .route("/health_check", get(health_check))
-    .route("/echo", post(echo));
+        .route("/", get(hello_world))
+        .route("/health_check", get(health_check))
+        .route("/webhook", post(webhook_handler));
     
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
-    .await
-    .expect("Failed to bind to port 3000");
+        .await
+        .expect("Failed to bind to port 3000");
 
+    println!("Listening for Vault tx on http://127.0.0.1:3000");
     axum::serve(listener, app)
-    .await
-    .expect("Server failed to start");
+        .await
+        .expect("Server failed to start");
 }
 
 async fn hello_world() -> &'static str {
@@ -27,6 +32,7 @@ async fn health_check() -> &'static str {
     "OK"
 }
 
-async fn echo(body: String) -> String {
-    format!("You sent: {}", body)
+async fn webhook_handler(Json(payload): Json<Value>) -> StatusCode {
+    println!("Transaction received:\n{:#}\n", payload);
+    StatusCode::OK
 }
